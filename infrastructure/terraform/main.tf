918c82a7bcfa0272bbf0ac4317cdd18209112463
Pedro Cardoso <pedro_ca_03@outlook.com>
2026-06-08 23:56:27 +0100

    fix: EC2 IP and use AMI data source

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -21,6 +21,17 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -99,11 +110,12 @@ resource "aws_security_group" "sanity_sg" {
 
 # Resource for sanity check EC2 instance
 resource "aws_instance" "sanity_ec2" {
-  ami           = "ami-0c7217cdde317cfec" 
-  instance_type = "t3.micro"
-  key_name      = "microservices-key"
-  subnet_id     = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.sanity_sg.id]
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  key_name                    = "microservices-key"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.sanity_sg.id]
+  associate_public_ip_address = true # Garante a atribuição de IP público
 
   tags = merge(var.common_tags, {
     Name = "${var.project_name}-sanity-ec2"
